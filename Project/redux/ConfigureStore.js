af32e794454c8b82c0070fdd447d85e8476d2398
THH12062002 <thh12062002@gmail.com>
2023-12-11 21:05:44 +0700

    done maybe all

---
+++ Project/redux/ConfigureStore.js
@@ -0,0 +1,24 @@
+// redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+// reducers
+import { leaders } from './leaders';
+import { dishes } from './dishes';
+import { comments } from './comments';
+import { promotions } from './promotions';
+import { favorites } from './favorites';
+import { users } from './users';
+// redux-persist
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+const config = { key: 'root', storage: AsyncStorage, debug: true };
+
+export const ConfigureStore = () => {
+  const store = createStore(
+    persistCombineReducers(config, { leaders, dishes, comments, promotions, favorites, users }),
+    applyMiddleware(thunk, logger)
+  );
+  const persistor = persistStore(store);
+  return { persistor, store };
+};
